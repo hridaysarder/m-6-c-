5696db02ec2608f9ceb442151383f14ab3f88068
HridaySarder <hridoyaug12@gmail.com>
2024-06-07 11:05:24 +0600

    first commit

---
+++ string_constructor.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s="Hello World";
+    // string s("Hello Bangladesh");
+    // string s("Hello World",7);
+    string a="hello world";
+    // string s(a,4);
+    string s(20,'A');
+    cout<<s <<endl;
+    return 0;
+}
